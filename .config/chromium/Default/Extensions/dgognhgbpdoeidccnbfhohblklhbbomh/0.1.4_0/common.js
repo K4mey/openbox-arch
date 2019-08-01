7e55f9e23b43b94347d21efd65604ed465ad21e3
Your Name <you@example.com>
2019-08-01 20:17:50 +0000

    update files

---
+++ .config/chromium/Default/Extensions/dgognhgbpdoeidccnbfhohblklhbbomh/0.1.4_0/common.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var set = () => chrome.cookies.set({
+  url: 'https://www.youtube.com',
+  domain: '.youtube.com',
+  name: 'wide',
+  value: '1'
+});
+chrome.runtime.onInstalled.addListener(set);
+chrome.runtime.onStartup.addListener(set);
+let id;
+chrome.cookies.onChanged.addListener(info => {
+  if (info.cookie.name === 'wide' && info.cookie.value !== '1') {
+    window.clearTimeout(id);
+    id = window.setTimeout(set, 1000);
+  }
+});
+
+{
+  const {onInstalled, setUninstallURL, getManifest} = chrome.runtime;
+  const {name, version} = getManifest();
+  const page = getManifest().homepage_url;
+  onInstalled.addListener(({reason, previousVersion}) => {
+    chrome.storage.local.get({
+      'faqs': true,
+      'last-update': 0
+    }, prefs => {
+      if (reason === 'install' || (prefs.faqs && reason === 'update')) {
+        const doUpdate = (Date.now() - prefs['last-update']) / 1000 / 60 / 60 / 24 > 45;
+        if (doUpdate && previousVersion !== version) {
+          chrome.tabs.create({
+            url: page + '&version=' + version +
+              (previousVersion ? '&p=' + previousVersion : '') +
+              '&type=' + reason,
+            active: reason === 'install'
+          });
+          chrome.storage.local.set({'last-update': Date.now()});
+        }
+      }
+    });
+  });
+  setUninstallURL(page + '&rd=feedback&name=' + encodeURIComponent(name) + '&version=' + version);
+}
