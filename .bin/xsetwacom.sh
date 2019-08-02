95460d3a5480552d156086efdf6b392a28f2c211
Your Name <you@example.com>
2019-08-02 16:05:49 -0300

    update scripts and openbox autostart

---
+++ .bin/xsetwacom.sh
@@ -0,0 +1,4 @@
+xsetwacom set 9 MapToOutput 1920x1080+1920+0 &
+xsetwacom set 10 MapToOutput 1920x1080+1920+0 &
+xsetwacom set 18 MapToOutput 1920x1080+1920+0 &
+xsetwacom set 19 MapToOutput 1920x1080+1920+0 &
