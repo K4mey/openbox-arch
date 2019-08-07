d73d55af2442a242e4c61ea9c4a3d056f399863c
Your Name <you@example.com>
2019-08-06 23:47:15 -0300

    edit schema.pl from obmenu

--- .config/obmenu-generator/schema.pl
+++ .config/obmenu-generator/schema.pl
@@ -106,8 +106,8 @@ our $SCHEMA = [
 
     ## This option uses the default Openbox's action "Exit"
     {exit => ['Exit', 'exit']},
-	{poweroff => ['poweroff', 'poweroff']},
-	{reboot => ['reboot', 'reboot']},
+	{exit => ['Poweroff', 'poweroff']},
+	{exit => ['Reboot', 'reboot']},
     ## This uses the 'oblogout' menu
     # {item => ['oblogout', 'Exit', 'exit']},
 ]
