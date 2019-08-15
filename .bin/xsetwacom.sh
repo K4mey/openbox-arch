bcd6b56c1cd004a7103396b8233174cc007daf05
Your Name <you@example.com>
2019-08-15 01:32:07 -0300

    update xsetwacom.sh and autostart

--- .bin/xsetwacom.sh
+++
@@ -1,4 +0,0 @@
-xsetwacom set 9 MapToOutput 1920x1080+1920+0 &
-xsetwacom set 10 MapToOutput 1920x1080+1920+0 &
-xsetwacom set 18 MapToOutput 1920x1080+1920+0 &
-xsetwacom set 19 MapToOutput 1920x1080+1920+0 &
