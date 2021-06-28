4e7172d9b45129c391843242d6a54d0fa5811542
Kamey <48242163+K4mey@users.noreply.github.com>
2021-06-28 10:31:05 +0200

    Update config.pl

--- .config/obmenu-generator/config.pl
+++ .config/obmenu-generator/config.pl
@@ -24,6 +24,7 @@
                             ],
 
 || ICON SETTINGS
+    | use_gtk3            : Use the Gtk3 library for resolving the icon paths. (default: 0)
     | gtk_rc_filename     : Absolute path to the GTK configuration file.
     | missing_icon        : Use this icon for missing icons (default: gtk-missing-image)
     | icon_size           : Preferred size for icons. (default: 48)
@@ -69,7 +70,7 @@ our $CONFIG = {
                            },
   "locale_support"      => 1,
   "missing_icon"        => "gtk-missing-image",
-  "terminal"            => "termite",
+  "terminal"            => "xfce4-terminal",
   "use_gtk3"            => 0,
-  "VERSION"             => 0.86,
+  "VERSION"             => 0.89,
 }
